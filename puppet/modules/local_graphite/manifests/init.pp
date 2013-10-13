ef3ffc51982a994b459898d62f4151ebda2f626e
Adriaan Thomas <athomas@xebia.com>
2013-10-13 19:15:39 +0200

    Added graphTemplates.conf, rearranged code a bit.

--- puppet/modules/local_graphite/manifests/init.pp
+++ puppet/modules/local_graphite/manifests/init.pp
@@ -1,49 +1,11 @@
 # Helper class for our CentOS Graphite install.
 # Makes a few assumptions, not very modular.
 # Note that we also modify some settings through hiera.
-class local_graphite (
-  $sincedb_path = hiera('logstash::sincedb_path')
-) {
+class local_graphite {
   include graphite
 
   Class['graphite'] ~> Service['httpd']
 
-  file { '/usr/lib/python2.6/site-packages/graphite/initial_data.json':
-    content => template("${module_name}/initial_data.json.erb"),
-    owner   => 'apache',
-    group   => 'apache',
-    mode    => 0400,
-    require => Package['graphite-web'],
-  } ->
-
-  exec { 'syncdb':
-    cwd     => '/usr/lib/python2.6/site-packages/graphite',
-    command => '/usr/bin/python manage.py syncdb --noinput',
-    user    => 'apache',
-    creates => '/var/lib/graphite-web/graphite.db',
-    before  => Service['httpd'],
-  }
-
-  file { '/etc/httpd/conf.d/graphite-web.conf':
-    source  => "puppet:///modules/${module_name}/graphite-web.conf",
-    owner   => root,
-    group   => root,
-    mode    => 0444,
-    notify  => Service['httpd'],
-    require => Package['graphite-web'],
-  }
-
-  logstash::input::file { 'graphite-httpd-logs':
-    path         => ['/var/log/httpd/graphite-web-access.log', '/var/log/httpd/graphite-web-error.log'],
-    type         => 'access-log',
-    sincedb_path => $sincedb_path,
-  }
-
-  logstash::input::file { 'graphite-web-logs':
-    path         => ['/var/log/graphite-web/*.log'],
-    type         => 'graphite-web',
-    sincedb_path => $sincedb_path,
-  }
-
   include local_graphite::carbon::config
+  include local_graphite::web::config
 }
---
+++ puppet/modules/local_graphite/manifests/web/config.pp
@@ -0,0 +1,48 @@
+class local_graphite::web::config (
+  $sincedb_path = hiera('logstash::sincedb_path')
+) {
+  File {
+    owner => root,
+    group => root,
+    mode  => 0444,
+  }
+
+  file { '/usr/lib/python2.6/site-packages/graphite/initial_data.json':
+    content => template("${module_name}/initial_data.json.erb"),
+    owner   => 'apache',
+    group   => 'apache',
+    mode    => 0400,
+    require => Package['graphite-web'],
+  } ->
+
+  exec { 'syncdb':
+    cwd     => '/usr/lib/python2.6/site-packages/graphite',
+    command => '/usr/bin/python manage.py syncdb --noinput',
+    user    => 'apache',
+    creates => '/var/lib/graphite-web/graphite.db',
+    before  => Service['httpd'],
+  }
+
+  file { '/etc/graphite-web/graphTemplates.conf':
+    source => "puppet:///modules/${module_name}/graphTemplates.conf",
+    before => Service['httpd'], # no restart required?
+  }
+
+  file { '/etc/httpd/conf.d/graphite-web.conf':
+    source  => "puppet:///modules/${module_name}/graphite-web.conf",
+    notify  => Service['httpd'],
+    require => Package['graphite-web'],
+  }
+
+  logstash::input::file { 'graphite-httpd-logs':
+    path         => ['/var/log/httpd/graphite-web-access.log', '/var/log/httpd/graphite-web-error.log'],
+    type         => 'access-log',
+    sincedb_path => $sincedb_path,
+  }
+
+  logstash::input::file { 'graphite-web-logs':
+    path         => ['/var/log/graphite-web/*.log'],
+    type         => 'graphite-web',
+    sincedb_path => $sincedb_path,
+  }
+}
